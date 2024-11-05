c744064865b41e59d177d7cc4c3206eb3d304f9a
kadam1222 <k.adam12220@gmail.com>
2024-11-05 13:29:21 +0100

    3.ütem kezdete

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let honap = d.getMonth()+1;
 let datum = document.getElementById("datum");
 datum.innerHTML= ev + ". " +honap+". hónap"
 
-
+let honaphossz=[31,28,31,30,31,30,31,31,30,31,30,31]
 let fodiv=document.getElementById("fodiv");
 let napok_div=document.getElementById("napok")
 let napok=["H","K","Sze","Cs","P","Szo","V"]
@@ -21,14 +21,42 @@ elozo.addEventListener("click",function(){
         ev=ev-1
         honap=12
     }
-    datum.innerHTML= ev + ". " +honap+". hónap" 
+    datum.innerHTML= ev + ". " +honap+". hónap"
+    nap.innerText="";
+    for(let i=1;i<honaphossz[honap-1]+1;i++){
+        let jelenleg = document.createElement("button")
+        jelenleg.innerText=i
+        if(i==1){
+            jelenleg.id="elso"
+        }
+        nap.appendChild(jelenleg)
+    }
+    fodiv.appendChild(nap)
 })
 
-
+let kov = document.getElementById("kov");
+kov.addEventListener("click",function(){
+    honap= honap+1;
+    if(honap==13){
+        ev=ev+1
+        honap=1
+    }
+    datum.innerHTML= ev + ". " +honap+". hónap"
+    nap.innerText="";
+    for(let i=1;i<honaphossz[honap-1]+1;i++){
+        let jelenleg = document.createElement("button")
+        jelenleg.innerText=i
+        if(i==1){
+            jelenleg.id="elso"
+        }
+        nap.appendChild(jelenleg)
+    }
+    fodiv.appendChild(nap)
+})
 
 let nap = document.createElement("div");
 nap.id="naplist"
-for(let i=1;i<32;i++){
+for(let i=1;i<31;i++){
     let jelenleg = document.createElement("button")
     jelenleg.innerText=i
     if(i==1){
